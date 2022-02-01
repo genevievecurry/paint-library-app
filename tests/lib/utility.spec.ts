3d61a1c1cd9472f6620b2062aa943ce29854cf2b
Genevieve Curry <gcurry@protonmail.com>
2022-02-01 15:07:48 -0800

    Fixed the single test, as it was singularly failing.

--- tests/lib/utility.spec.ts
+++ tests/lib/utility.spec.ts
@@ -5,19 +5,19 @@ import { pigmentCode } from '../../src/lib/utility';
 
 const Utility = suite('/lib/utility');
 
-let pigmentType: string, pigmentNumber: string, colorCode: string;
+let pigmentType: string, pigmentNumber: number, colorCode: string;
 
 Utility.before(ENV.setup);
 Utility.before.each(ENV.reset);
 
 Utility.before(() => {
   pigmentType = 'CIPIGMENT';
-  pigmentNumber = '62';
+  pigmentNumber = 62;
   colorCode = 'Br';
 });
 
 Utility('should return a converted pigment ci code', () => {
-  assert.is(pigmentCode(pigmentType, pigmentNumber, colorCode), 'PBr62');
+  assert.is(pigmentCode(pigmentType, pigmentNumber, colorCode, {html: false}), 'PBr62');
 });
 
 Utility.run();
