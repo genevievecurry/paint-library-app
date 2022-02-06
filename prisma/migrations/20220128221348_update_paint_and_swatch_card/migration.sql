510646b344b6fbc6d3e0d673ff96cedf099fbf02
Genevieve Curry <gcurry@protonmail.com>
2022-02-06 14:34:29 -0800

    Trying again

--- prisma/migrations/20220128221348_update_paint_and_swatch_card/migration.sql
+++ prisma/migrations/20220128221348_update_paint_and_swatch_card/migration.sql
@@ -11,8 +11,8 @@ ALTER COLUMN "communityDescription" SET DATA TYPE VARCHAR(1000),
 ALTER COLUMN "manufacturerDescription" SET DATA TYPE VARCHAR(1000);
 
 -- AlterTable
-ALTER TABLE "SwatchCard" ADD COLUMN     "isOriginal" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "primaryOnPaintUuid" TEXT IF NOT EXISTS;
+ALTER TABLE "SwatchCard" ADD COLUMN IF NOT EXISTS "isOriginal" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN IF NOT EXISTS "primaryOnPaintUuid" TEXT;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "SwatchCard_primaryOnPaintUuid_key" ON "SwatchCard"("primaryOnPaintUuid");
